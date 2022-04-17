89bb90038db5d9111ebf36977dae5b232293b3fd
Stephen Park <40534021@live.napier.ac.uk>
2022-04-17 07:26:38 +0100

    updating json format

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 var network_types_bttn = document.getElementById("network_types_quiz_bttn");
 var common_attacks_bttn = document.getElementById("common_attacks_quiz_bttn");
 var firewall_bttn = document.getElementById("firewall_quiz_bttn");
-var json_link = "https://stephenpark2000.github.io/WebTech/question_bank.json"
+var json_link = "https://stephenpark2000.github.io/WebTech/scripts/question_bank.json"
 
 quiz_open()
 //Opens the relevant quiz as object
@@ -14,7 +14,7 @@ function quiz_open(){
 
     let quiz_chosen = sessionStorage.getItem("quiz_id");
     console.log(quiz_chosen)
-    console.log(question_bank)
+    console.log(question_bank["quiz"])
 
 }
 
