c78c0082bcf26acd000e905b6b14a807b85598a7
Stephen Park <40534021@live.napier.ac.uk>
2022-04-17 07:04:41 +0100

    updating json location

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,21 @@
-const network_services_bttn = document.getElementById("network_services_quiz_bttn");
-const network_types_bttn = document.getElementById("network_types_quiz_bttn");
-const common_attacks_bttn = document.getElementById("common_attacks_quiz_bttn");
-const firewall_bttn = document.getElementById("firewall_quiz_bttn");
 
-network_services_bttn.addEventListener("click", quiz_selection);
+var network_types_bttn = document.getElementById("network_types_quiz_bttn");
+var common_attacks_bttn = document.getElementById("common_attacks_quiz_bttn");
+var firewall_bttn = document.getElementById("firewall_quiz_bttn");
+var json_link = "https://stephenpark2000.github.io/WebTech/question_bank.json"
 
-//identifies where the user came from and redirects to quiz.html
-function quiz_selection(evt){
-    var id = evt.target.id
-    if (id = network_services_bttn){
-        var quiz = "network_services_quiz";
-        location.href="quiz.html";
-        return(quiz) 
-    }
-    else {
-        window.alert("There has been an error please come back later") 
-    }
+quiz_open()
+//Opens the relevant quiz as object
+function quiz_open(){
+    var question_bank = fetch(json_link)
+        .then(response => {return response.json()})
+        .then(jsondata => console.log(jsondata));
+
+
+    let quiz_chosen = sessionStorage.getItem("quiz_id");
+    console.log(quiz_chosen)
+    console.log(question_bank)
+
 }
+
+
